c5e707baccfa51a5ef399de6e72254ca2563a7ee
nitin.eusebius@gmail.com <nitin.eusebius@gmail.com>
2016-11-24 10:59:38 -0800

    react skeleton

---
+++ src/main.jsx
@@ -0,0 +1,6 @@
+var React = require('react');
+var ReactDOM = require('react-dom');
+
+var ListA = require('./components/List.jsx');
+
+ReactDOM.render(<ListA />,document.getElementById('ingredient'));
